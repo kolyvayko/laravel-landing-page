d97fa793b566d80eb0a0e0052e4712e833077201
Pavol Senko <pavol.senko@socialisten.at>
2016-08-19 16:01:34 +0200

    add controller to submit contact forms

---
+++ app/Http/Controllers/ContactFormController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Resque;
+
+class ContactFormController extends Controller
+{
+
+    /**
+     * Verifies input and sends an email
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function postContact()
+    {
+
+    }
+
+}
