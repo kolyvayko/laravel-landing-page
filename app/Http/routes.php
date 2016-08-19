0cbfb9c98f15468aae8bd057616afa6a75979860
Pavol Senko <pavol.senko@socialisten.at>
2016-08-19 16:02:23 +0200

    setup basic routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(["middleware" => ["web"]], function () {
+
+    // route to homepage
+    Route::get("/", ["as" => "getHome", "uses" => "PagesController@getHome"]);
+
+    // post route to send email from contact form
+    Route::post("/contact", ["before" => "csrf", "as" => "postContact", "uses" => "ContactFormController@postContact"]);
+
+    // sitemap.xml route
+    Route::get("/sitemap.xml", ["as" => "sitemap", "uses" => "SitemapController@getSitemapXml"]);
+
+    // robots.txt route
+    Route::get("/robots.txt", ["as" => "robots", "uses" => "RobotsController@getRobotsTxt"]);
+
+    // any other route that gets page based on the url
+    Route::get("/{url}", ["as" => "getPage", "uses" => "PagesController@getPage"])
+        ->where("url", "[0-9, a-z, A-Z, \-]+");
 });
